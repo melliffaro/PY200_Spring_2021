a00b4b5867d55ee07f5d12911eadd52bea5c0bff
aeksei <aeksei94@gmail.com>
2021-02-24 13:38:11 +0300

    refactor table

--- lesson_6/table.py
+++ lesson_6/table.py
@@ -19,9 +19,19 @@ class Table:
         self.db_driver = db_driver
         self.fields = fields
 
+        self.__sql = self.__initial_sql()
         self.create_table()
 
-    def create_table(self):
+    def __initial_sql(self):
+        """
+        CREATE TABLE IF NOT EXISTS table_name (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,  -- первичный ключ, в нашей ORM всегда такой
+
+            title TEXT,
+            price REAL,
+            pages INTEGER
+        );
+        """
         sql = f"CREATE TABLE IF NOT EXISTS {self.table_name} (\n"
         sql += f"\t id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
 
@@ -31,11 +41,19 @@ class Table:
 
         # sql += ",\n".join(f"\t {field} {self.SQLITE_FIELD[type_]}" for field, type_ in self.fields.items())
         # sql += "\n);"
-        print(sql)
+
+        return sql
+
+    @property
+    def sql(self):
+        return self.__sql
+
+    def create_table(self):
         with self.db_driver as cursor:
-            cursor.execute(sql)
+            cursor.execute(self.__sql)
 
 
 if __name__ == '__main__':
     driver = SqliteDB('library.sqlite3')
-    book_table = Table(driver, 'books', title=int, price=float)
+    book_table = Table(driver, 'books', title=str, price=float, pages=int)
+    print(book_table.sql)
